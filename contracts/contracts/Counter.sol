0b103205d50d18f94927cfe09190778935b1e1b6
widiskel <waputra16@gmail.com>
2025-02-19 11:13:03 +0700

    feat (counter) add decrement counter

--- contracts/contracts/Counter.sol
+++ contracts/contracts/Counter.sol
@@ -5,13 +5,20 @@ contract Counter {
     uint256 private count;
     
     event CountIncremented(uint256 newCount);
+    event CountDecremented(uint256 newCount);
     
     function increment() public {
         count += 1;
         emit CountIncremented(count);
     }
     
+    function decrement() public {
+        require(count > 0, "Count cannot be negative");
+        count -= 1;
+        emit CountDecremented(count);
+    }
+    
     function getCount() public view returns (uint256) {
         return count;
     }
-} 
+}
